d2abfff2039cab86f487149b403b70126163a879
Rotem-Mokady <rotemmokady@gmail.com>
2024-03-20 20:31:04 +0200

    finish ex1 without the two last questions

--- exercise_1/ex1_322640210.py
+++ exercise_1/ex1_322640210.py
@@ -1,49 +1,77 @@
 ''' Exercise #1 '''
+from typing import Union
 
 #########################################
 # Question 1 - do not delete this comment
 #########################################
-def hi_name(name):
+
+
+def hi_name(name: str) -> None:
     # Write the rest of the code for question 1 below here.
+    print(f"Hi {name.title()}{3*'!'}")
 
 
 #########################################
 # Question 2 - do not delete this comment
 #########################################
 
-def deltoid(AB, AD, ABD, ABC):
+def deltoid(AB: float, AD: float, ABD: float, ABC: float) -> None:
     # Write the rest of the code for question 2 below here.
+    perimeter = 2 * (AB + AD)
+    print(f"Perimeter is: {perimeter}")
+
+    ac, bd = ABC - 2 * AB, ABD - AB - AD
+    print(f"AC: {ac}")
+    print(f"BD: {bd}")
+
+    s = ac * bd / 2
+    print(f"S: {s}")
 
 
 #########################################
 # Question 3 - do not delete this comment
 #########################################
-def divide_by_seven(num): 
+def divide_by_seven(num: str) -> Union[int, float, None]:
     # Write the rest of the code for question 3 below here.
+    divided_by = 7
+    _num = int(num)
 
+    if not _num % divided_by:
+        print(f"The number {num} is divisible by {divided_by}")
+        return _num / divided_by
+
+    print(f"The number {num} is not divisible by {divided_by}")
 
 
 #########################################
 # Question 4 - do not delete this comment
 #########################################
-def str_mixer(text, copies1, copies2): 
+def str_mixer(text: str, copies1: int, copies2: int) -> None:
     # Write the rest of the code for question 4 below here.
+    sub1, sub2 = '', ''
 
+    for idx, char in enumerate(text):
+        if not idx % 2:
+            sub1 += char
+        else:
+            sub2 += char
+
+    print(f"{copies1 * sub1}{copies2 * sub2}")
 
 
 #########################################
 # Question 5 - do not delete this comment
 #########################################
-def rearrange_str(name, ind1, ind2): 
+def rearrange_str(name: str, ind1: int, ind2: int):
     # Write the rest of the code for question 5 below here.
-
+    pass
 
 #########################################
 # Question 6 - do not delete this comment
 #########################################
 def divisor_checker(digits, divisor): 
     # Write the rest of the code for question 6 below here.
-
+    pass
 
 #########################################
 
@@ -57,7 +85,7 @@ if __name__=='__main__':
     deltoid(10.0, 5.0, 28.0, 26.0)
 
     # Ex3 tests
-    divide_by_seven('7')
+    divide_by_seven('77')
     divide_by_seven('-17')
 
     # Ex4 tests
@@ -73,4 +101,3 @@ if __name__=='__main__':
     # Ex6 tests
     divisor_checker('428315', 4)
     divisor_checker('428315', 5)
-    
