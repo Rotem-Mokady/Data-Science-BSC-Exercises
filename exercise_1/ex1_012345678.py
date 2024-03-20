7582cea62cdb340b65bb4d7cac162994804cdc43
Rotem-Mokady <rotemmokady@gmail.com>
2024-03-20 19:38:42 +0200

    initial commit

---
+++ exercise_1/ex1_012345678.py
@@ -0,0 +1,76 @@
+''' Exercise #1 '''
+
+#########################################
+# Question 1 - do not delete this comment
+#########################################
+def hi_name(name):
+    # Write the rest of the code for question 1 below here.
+
+
+#########################################
+# Question 2 - do not delete this comment
+#########################################
+
+def deltoid(AB, AD, ABD, ABC):
+    # Write the rest of the code for question 2 below here.
+
+
+#########################################
+# Question 3 - do not delete this comment
+#########################################
+def divide_by_seven(num): 
+    # Write the rest of the code for question 3 below here.
+
+
+
+#########################################
+# Question 4 - do not delete this comment
+#########################################
+def str_mixer(text, copies1, copies2): 
+    # Write the rest of the code for question 4 below here.
+
+
+
+#########################################
+# Question 5 - do not delete this comment
+#########################################
+def rearrange_str(name, ind1, ind2): 
+    # Write the rest of the code for question 5 below here.
+
+
+#########################################
+# Question 6 - do not delete this comment
+#########################################
+def divisor_checker(digits, divisor): 
+    # Write the rest of the code for question 6 below here.
+
+
+#########################################
+
+if __name__=='__main__':
+
+    # Ex1 tests
+    hi_name('Tom')
+    hi_name('oxana')
+
+    # Ex2 tests
+    deltoid(10.0, 5.0, 28.0, 26.0)
+
+    # Ex3 tests
+    divide_by_seven('7')
+    divide_by_seven('-17')
+
+    # Ex4 tests
+    str_mixer('tom', 3, 4)
+    str_mixer('oxana', 2, 3)
+
+    # Ex5 tests
+    rearrange_str('ibaccaMleTvivA', 7, 10)
+    rearrange_str('ibaccaMleTvivA', 6, 30)
+    rearrange_str('ibaccaMleTvivA', 8, 7)
+    rearrange_str('', 7, 10)
+
+    # Ex6 tests
+    divisor_checker('428315', 4)
+    divisor_checker('428315', 5)
+    
